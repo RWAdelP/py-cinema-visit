119a8b57cbb86b53bd4bd0a5901e8873b2501eb4
Adel Pereira <nic.gis.office@gmail.com>
2026-04-20 13:00:16 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
+def cinema_visit(movie: str,
+                 customers: list,
                  hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+                 cleaner: str) -> None:
     customer_list = [Customer(cust["name"],
                               cust["food"]) for cust in customers]
     for customer in customer_list:
